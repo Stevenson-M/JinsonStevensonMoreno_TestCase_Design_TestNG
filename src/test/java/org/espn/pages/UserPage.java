02c9c66c5a3190680fee7bf4a45af4bcb3111a04
jinson Stevenson Moreno Aguilar <jinson.moreno@gmail.com>
2022-11-13 11:12:37 -0500

    Codigo inicial de test de desactivacion de cuenta

--- src/test/java/org/espn/pages/UserPage.java
+++ src/test/java/org/espn/pages/UserPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class UserPage extends BasePage {
 
     @FindBy(css="div.global-user:last-child ul.account-management > li:last-child > a")
@@ -14,6 +17,25 @@ public class UserPage extends BasePage {
 
     @FindBy (css = "div.global-user:last-child ul.account-management li.display-user")
     private WebElement NavUserMenuWelcomeText;
+
+    @FindBy(css="li:nth-child(2) a[tref='/members/v3_1/modifyAccount']")
+    private WebElement espnProfile;
+
+    String SettingsIframe = "oneid-iframe";
+
+    @FindBy(id="AccountDeleteLink")
+    private WebElement DeactivateAccountButton;
+
+    @FindBy(id="BtnSubmit")
+    private WebElement DeactivateAccountConfirmationButton;
+
+    @FindBy(xpath = "root > div:nth-child(3) > div > div > div:nth-child(1)")
+    private WebElement DeactivateModalMessageWindow;
+
+    @FindBy(id = "BtnSubmit")
+    private WebElement DeactivateModalMessageWindowButtonConfirmation;
+
+
 
     public void clickLoginButton() {
         clickElement(loginUserMenuButton);
@@ -31,8 +53,50 @@ public class UserPage extends BasePage {
         return loginUserMenuButton.isEnabled();
     }
 
+    public void clickEspnProfile() {
+        clickElement(espnProfile);
+    }
+
+    public void changeToSettingsIframe(){
+        getDriver().switchTo().frame(SettingsIframe);
+    }
+
+    public void clickDeactivateAccountButton() {
+        clickElement(DeactivateAccountButton);
+    }
+
+    public void clickDeactivateAccountConfirmationButton() {
+        clickElement(DeactivateAccountConfirmationButton);
+    }
+    String PrincipalWindow = getDriver().getWindowHandle();
+    public void switchToConfirmationDeactivatePopUp(){
+        waitForVisibility(DeactivateModalMessageWindow);
+
+        Set<String> windowHandles = getDriver().getWindowHandles();
+        Iterator<String> i1 = windowHandles.iterator();
+
+        while (i1.hasNext()) {
+            String ChildWindow = i1.next();
+            if (!PrincipalWindow.equalsIgnoreCase(ChildWindow)) {
+                getDriver().switchTo().window(ChildWindow);
+
+                getDriver().close();
+            }
+        }
+        getDriver().switchTo().window(PrincipalWindow);
+    }
+
+    public boolean checkDeactivateModalMessageWindowIsPresent(){
+        return DeactivateModalMessageWindow.isDisplayed();
+    }
+
+    public void clickDeactivateModalMessageWindowButtonConfirmation() {
+        clickElement(DeactivateModalMessageWindowButtonConfirmation);
+    }
+
     public UserPage(WebDriver driver) {
         super(driver);
     }
+
 
 }
--- src/test/java/org/espn/tests/EspnDeactivateAccountTest.java
+++ src/test/java/org/espn/tests/EspnDeactivateAccountTest.java
@@ -1,4 +1,41 @@
 package org.espn.tests;
 
-public class EspnDeactivateAccountTest {
+import org.espn.pages.BasePage;
+import org.espn.pages.LoginPage;
+import org.espn.pages.UserPage;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.is;
+
+public class EspnDeactivateAccountTest extends BaseTest {
+
+    @Test
+    @Parameters({"email", "password"})
+    public void EspnDeactivateAccountTest(String email, String password) {
+        BasePage mainPage = new BasePage(driver.getDriver());
+        mainPage.clickGlobalUserMenu();
+        UserPage userPage = new UserPage(driver.getDriver());
+        userPage.clickLoginButton();
+        LoginPage loginPage = new LoginPage(driver.getDriver());
+        loginPage.changeToIframe();
+        loginPage.clickEmailInput();
+        loginPage.typeEmailInput(email);
+        loginPage.clickPasswordInput();
+        loginPage.typePasswordInput(password);
+        loginPage.clickLoginButton();
+        BasePage mainPage2 = new BasePage(driver.getDriver());
+        mainPage2.hoverGlobalUserMenu();
+        UserPage userPage2 = new UserPage(driver.getDriver());
+        userPage2.clickEspnProfile();
+        userPage2.changeToSettingsIframe();
+        userPage2.clickDeactivateAccountButton();
+        userPage2.clickDeactivateAccountConfirmationButton();
+
+
+        /** TO WORK ON **/
+        /** userPage2.switchToConfirmationDeactivatePopUp();
+        checkThat("Deactivate modal message window is present", userPage2.checkDeactivateModalMessageWindowIsPresent(), is(true));
+        userPage2.clickDeactivateModalMessageWindowButtonConfirmation(); **/
+
+    }
 }
